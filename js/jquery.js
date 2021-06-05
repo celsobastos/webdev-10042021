b3998fae3432060ae1c27918710337ffa528d3bd
= <=>
2021-06-05 15:50:06 -0300

    jquery

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,96 @@
 $(function () {
 
-    $(".webdev").css("color", "red");
+    /*
+    $(".card").on("mouseover", function () {
+        $(this).fadeOut("slow").slideDown("slow").css("");
+    });
+
+
+    $(".card").on({
+        mouseover: function () {
+            $(this).fadeOut("slow")
+        },
+        mouseout: function () {
+            $(this).fadeIn("slow")
+        },
+        click: function () {
+            alert("funcionou");
+        },
+    });
+    */
+
+    /*
+
+    $( "#book" ).fadeIn( "slow", function() {
+        // Animation complete
+      });
+*/
+
+
+    /*
+    
+        $(".webdev").on({
+            mouseover: function () {
+                $(this).css("color", "red");
+            },
+            mouseout: function () {
+                $(this).css("color", "black");
+            },
+            click: function () {
+                alert("funcionou");
+            },
+        });
+    */
+    /*
+    function greet( event ) {
+        alert( "Hello " + event.data.name );
+      }
+      $( "button" ).on( "click", {
+        name: "Karl"
+      }, greet );
+
+      $( "button" ).on( "click", {
+        name: "Addy"
+      }, greet );
+*/
+    /*
+    $(".webdev").on("click", function () {
+        alert("ok");
+    });
+    */
+
+    /*
+    $(".webdev").click(function () {
+        alert("ok");
+    });
+    */
+
+    //$(".webdev").html("<a hre='#'>Meu novo link</a>");
+
+    /*
+    let attr = $(".webdev").attr("class");
+    console.log(attr);
+    /*
+
+    //let valor = $(".login").val();
+
+    //let text = $(".webdev").text();
+    //console.log(text);
+
+    // let html = $(".webdev").html();
+    //console.log(html);
+
+
+
+
+
+    /*
+    $("a", $(".card")).css({
+        "color": "red",
+        "font-size": "25px",
+        "display": "block",
+        "background-color": "blue"
+    });
+    */
 
 });
